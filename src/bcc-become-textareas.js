65507cbd244cd392b8cb518869eff6ce7b19a000
Ben Summers <bsummers@lokion.com>
2021-03-18 14:09:56 -0500

    Added comments

--- src/bcc-become-textareas.js
+++ src/bcc-become-textareas.js
@@ -1,17 +1,25 @@
 (function() {
+
+    // Various selectors inside the BCC that are hidden and would be nice if they were revealed as <textareas>
     const selectors = [
         '#htmlEdit_data',
         '#htmlEdit_description',
         '#htmlEdit_longDescription',
     ];
 
+    // Track if a replacement was made.
     let didReplaceAppletWithTextarea = false;
 
+    // A function to run on each selector to turn it into a textarea.
     const replaceAppletsWithTextareas = (selector) => {
         const input = document.querySelector(selector);
+
         if (!input) {
+            // It is not on the page, this code can't do anything further.
             return;
         }
+
+        // Turn the hidden input into a <textarea>
         const name = input.getAttribute('name');
         const id = input.getAttribute('id');
         const value = input.getAttribute('value');
@@ -25,7 +33,11 @@
         didReplaceAppletWithTextarea = true;
     }
 
+    // Run the transformation function on each selector
     selectors.forEach(it => replaceAppletsWithTextareas(it));
+
+
+    // If a transformation occurred, then get rid of any java <applet> tags on the page
     if (didReplaceAppletWithTextarea) {
         // Remove applets and applet-adjacent stuff
         document.querySelectorAll('applet,.appletHome').forEach((el, i) => {
